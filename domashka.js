33dadda5a88cb08aaf30b37ed38d88decc5981bb
63Maks63 <81768695+63Maks63@users.noreply.github.com>
2021-04-02 22:36:59 +0300

    Add files via upload

---
+++ domashka.js
@@ -0,0 +1,40 @@
+console.log ("Hello Vetal!")
+
+console.log ("Задача №1")
+ console.log ("Переменная 1 = 5")
+ console.log ("Переменная 2 = 10")
+ console.log ("Провести сложение, умножение, деление и найти разницу")
+
+  
+  var a = 5
+  var b = 10
+var sum = a + b
+ console.log(sum)
+var um = a * b
+ console.log(um)
+var del = a / b
+ console.log(del)
+var min = a - b
+ console.log (min)
+
+
+ console.log ("Задача №2")
+ console.log ("Переменная 1 меньше чем переменная 2")
+
+console.log (a < b)
+console.log (a > b)
+console.log (a == b)
+console.log (a != b)
+  
+  console.log ("Задача №3")
+console.log ("квадрат гипотенузы треугольника с катетами 4 и 3 равна ***") 
+
+  var c = 4
+  var d = 3
+var kvad = c * c + d * d
+ console.log(kvad)
+ var rat = Math.pow(c, 2) + Math.pow(d, 2) 
+  console.log(rat)
+
+  
+
