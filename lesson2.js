380fd981364ce2fd9875d140f2c335aaa408e9eb
63Maks63 <81768695+63Maks63@users.noreply.github.com>
2021-04-06 15:01:15 +0300

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,47 @@
+console.log("Hello!")
+
+var person = {}
+person.a = 0
+person.b = 0
+
+var creature ={}
+creature.a = 5
+creature.b = 0
+
+console.log (person)
+console.log (creature)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var Person
+
+Person = Object.create 
